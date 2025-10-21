7923ffa803432cdd785df505170b9d1b06143061
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-10-21 20:15:37 -0300

    feat(cliente) adiciona funcionalidade de excluir o cliente

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -11,7 +11,12 @@ public class ClienteController {
     private Cliente cliente;
 
     public void excluirCliente(){
-
+        if(this.cliente == null){
+            System.out.println("Nenhum cliente foi incluido ainda");
+        }else{
+            System.out.println("Cliente excluido com sucesso!");
+            this.cliente = null;
+        }
     }
     public void mostrarCliente(){
         if(this.cliente == null){
@@ -22,12 +27,13 @@ public class ClienteController {
         }
     }
     public void criarCliente(){
-            this.cliente = new Cliente();
-            System.out.println("Digite o nome do cliente:");
-            this.cliente.setNome(sc.nextLine());
-            System.out.println("Digite o cpf:");
-            this.cliente.setCpf(sc.nextInt());
-            Main.limparTela();
+
+        this.cliente = new Cliente();
+        System.out.println("Digite o nome do cliente:");
+        this.cliente.setNome(sc.nextLine());
+        System.out.println("Digite o cpf:");
+        this.cliente.setCpf(sc.nextInt());
+        Main.limparTela();
 
     }
     public void mostrarMenu(){
@@ -36,13 +42,14 @@ public class ClienteController {
             System.out.println("1. Criar Cliente");
             System.out.println("2. Mostrar Cliente");
             System.out.println("3. Editar Cliente");
-            System.out.println("3. Excluir Cliente");
+            System.out.println("4. Excluir Cliente");
             opcao = sc.nextInt();
             sc.nextLine();
             Main.limparTela();
             switch (opcao){
                 case 1 -> criarCliente();
                 case 2 -> mostrarCliente();
+                case 4 -> excluirCliente();
             }
         }while(opcao!=0);
     }
